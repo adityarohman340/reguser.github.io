9b227fc508888a7fcddfe53eaa6fad7370e88fa5
Aditya <49444558+adityarohman340@users.noreply.github.com>
2019-05-26 19:01:28 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
 
                 if (count($registerants) > 0) {
                     echo "<h3 style='color: white; text-align: center; text-transform: uppercase;'>User yang telah terdaftar:</h3>";
-                    echo "<table class='table table-hover'><thead>";
+                    echo "<table class='table table-hover' style='color: white;'><thead>";
                     echo "<tr><th>Nama</th>";
                     echo "<th>Email</th>";
                     echo "<th>Profesi</th>";
